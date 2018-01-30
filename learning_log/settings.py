c8a154067b89cc433706675f943b36accdebfae3
ll_admin <ll_admin@gmail.com>
2018-01-30 16:11:39 +0800

    Correct settings.py:

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -152,8 +152,8 @@ if os.getcwd() == '/app':
     #静态资产配置
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-            os.path.join(BASE_DIR,'static'),
-            )
+    #STATICFILES_DIRS = (
+    #        os.path.join(BASE_DIR,'static'),
+    #        )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
